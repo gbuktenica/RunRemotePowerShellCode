dce09b988392d9f9f0d1cd242da2495630b15f67
Glen Buktenica <glen.buktenica@purple.telstra.com>
2021-06-03 14:22:54 +0800

    Roll back saved creds

--- Run-RemoteCode.ps1
+++ Run-RemoteCode.ps1
@@ -189,48 +189,34 @@ function Get-SavedCredentials {
 
     .PARAMETER Title
         The name of the username and password pair. This allows multiple accounts to be saved such as a normal account and an administrator account.
-        The default value is "Default"
 
     .PARAMETER VaultPath
         The file path of the encrypted Json file for saving the username and password pair.
-        Default value is "c:\users\<USERNAME>\SavedCredentialsHash.json"
+        Default value is c:\users\<USERNAME>\PowerShellHash.json"
 
     .PARAMETER Renew
         Prompts the user for a new password for an existing pair.
         To be used after a password change.
-        Default value is $false
-
-    .PARAMETER SecureString
-        Saves and returns a SecureString object instead of PSCredential Object.
-        Used for non Credential secrets.
-        Default value is $false
 
     .EXAMPLE
         Enter-PsSession -ComputerName Computer -Credential (Get-SavedCredentials)
-        Returns a default PsCredential object into the Enter-PsSession command.
 
     .EXAMPLE
-        $Credential = Get-SavedCredentials -Title Normal -VaultPath c:\temp\myFile.json
-        Returns a PsCredential object to to the variable $Credential
-
-    .EXAMPLE
-        $SecureString = Get-SavedCredentials -SecureString
-        Returns a SecureString object to to the variable $SecureString
+        $Credential = Get-SavedCredentials -Title Normal -VaultPath c:\temp\myfile.json
 
     .LINK
         https://github.com/gbuktenica/GetSavedCredentials
 
     .NOTES
         License      : MIT License
-        Copyright (c): 2021 Glen Buktenica
-        Release      : v1.1.0 20210413
+        Copyright (c): 2020 Glen Buktenica
+        Release      : v1.0.0 20200315
     #>
     [CmdletBinding()]
     Param(
         [string]$Title = "Default",
-        [string]$VaultPath = "$env:USERPROFILE\SavedCredentialsHash.json",
-        [switch]$Renew,
-        [switch]$SecureString
+        [string]$VaultPath = "$env:USERPROFILE\PowerShellHash.json",
+        [switch]$Renew
     )
     $JsonChanged = $false
     if (-not (Test-path -Path $VaultPath)) {
@@ -253,7 +239,7 @@ function Get-SavedCredentials {
         $Json | Add-Member -Name $Title -value (Convertfrom-Json $TitleContent) -MemberType NoteProperty
         $JsonChanged = $true
     }
-    if ($Json.$Title.username.Length -eq 0 -and -not $SecureString) {
+    if ($Json.$Title.username.Length -eq 0) {
         #Prompt user for username if it is not saved.
         $Message = "Enter User name for> $Title"
         $Username = Read-Host $Message -ErrorAction Stop
@@ -262,40 +248,25 @@ function Get-SavedCredentials {
     }
     if ($Json.$Title.password.Length -eq 0 -or $Renew) {
         #Prompt user for Password if it is not saved.
-        if ($SecureString) {
-            $Message = "Enter Secret for> " + $Json.$Title
-        } else {
-            $Message = "Enter Password for> " + $Json.$Title.username
-        }
-        $Json.$Title.password = ((Read-Host $Message -AsSecureString -ErrorAction Stop))
+        $Message = "Enter Password for> " + $Json.$Title.username
+        $secureStringPwd = Read-Host $Message -AsSecureString -ErrorAction Stop
+        $secureStringText = $secureStringPwd | ConvertFrom-SecureString
+        $Json.$Title.password = $secureStringText
         $JsonChanged = $true
     }
-    If ($SecureString) {
-        Try {
-            # Build the SecureString object and export it.
-            $Json.$Title.password | ConvertTo-SecureString -ErrorAction Stop
-        } catch {
-            # If building the SecureString failed for any reason delete it and run the function
-            # again which will prompt the user for the secret.
-            $TitleContent = " { `"username`":`"`", `"password`":`"`" }"
-            $Json | Add-Member -Name $Title -value (Convertfrom-Json $TitleContent) -MemberType NoteProperty -Force
-            $Json | ConvertTo-Json -depth 3 | Set-Content $VaultPath -ErrorAction Stop
-            Get-SavedCredentials -Title $Title -VaultPath $VaultPath -SecureString
-        }
-    } else {
-        $Username = $Json.$Title.username
-        Try {
-            # Build the PSCredential object and export it.
-            $SecurePassword = $Json.$Title.password | ConvertTo-SecureString -ErrorAction Stop
-            New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword -ErrorAction Stop
-        } catch {
-            # If building the credential failed for any reason delete it and run the function
-            # again which will prompt the user for username and password.
-            $TitleContent = " { `"username`":`"`", `"password`":`"`" }"
-            $Json | Add-Member -Name $Title -value (Convertfrom-Json $TitleContent) -MemberType NoteProperty -Force
-            $Json | ConvertTo-Json -depth 3 | Set-Content $VaultPath -ErrorAction Stop
-            Get-SavedCredentials -Title $Title -VaultPath $VaultPath
-        }
+
+    $Username = $Json.$Title.username
+    Try {
+        # Build the PSCredential object and export it.
+        $SecurePassword = $Json.$Title.password | ConvertTo-SecureString -ErrorAction Stop
+        New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword -ErrorAction Stop
+    } catch {
+        # If building the credential failed for any reason delete it and run the function
+        # again which will prompt the user for username and password.
+        $TitleContent = " { `"username`":`"`", `"password`":`"`" }"
+        $Json | Add-Member -Name $Title -value (Convertfrom-Json $TitleContent) -MemberType NoteProperty -Force
+        $Json | ConvertTo-Json -depth 3 | Set-Content $VaultPath -ErrorAction Stop
+        Get-SavedCredentials -Title $Title -VaultPath $VaultPath
     }
     If ($JsonChanged) {
         # Save the Json object to file if it has changed.
