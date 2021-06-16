952d8fed606ec79b919ed0e1239022484ff5a9e5
Glen Buktenica <glen.buktenica@kloud.com.au>
2021-06-16 16:56:19 +0800

    Verbose

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -15,8 +15,10 @@ if ($Secret.length -gt 0) {
     Write-Host "Creating credential object for $Username"
     $SecurePassword = $Secret | ConvertTo-SecureString -AsPlainText -Force
     $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword -ErrorAction Stop
+    Write-Host "Starting Pester Container with Credentials"
     $PesterContainer = New-PesterContainer -Path $TestPath -Data @{ 'Credential' = $Credential }
 } else {
+    Write-Host "Starting Pester Container"
     $PesterContainer = New-PesterContainer -Path $TestPath
 }
 $PesterResult = Invoke-Pester -Container $PesterContainer -ErrorAction Stop -Output Detailed -PassThru
