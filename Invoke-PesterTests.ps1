388fd6f51e35006f5641fd0e52d54ee2effaaed0
gbuktenica <glen@buktenica.com>
2021-06-17 08:52:30 +0800

    Diagnostic

--- Invoke-PesterTests.ps1
+++ Invoke-PesterTests.ps1
@@ -15,11 +15,12 @@ if ($Secret.length -gt 0) {
     Write-Host "Creating credential object for $Username"
     $SecurePassword = $Secret | ConvertTo-SecureString -AsPlainText -Force
     $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword -ErrorAction Stop
-    Write-Host "Starting Pester Container with Credentials"
+    Write-Host "Creating Pester Container with Credentials"
     $PesterContainer = New-PesterContainer -Path $TestPath -Data @{ 'Credential' = $Credential }
 } else {
-    Write-Host "Starting Pester Container"
+    Write-Host "Creating Pester Container"
     $PesterContainer = New-PesterContainer -Path $TestPath
 }
-$PesterResult = Invoke-Pester -Container $PesterContainer -ErrorAction Stop -Output Detailed -PassThru
+Write-Host "Invoking Pester Container"
+$PesterResult = Invoke-Pester -Container $PesterContainer -ErrorAction Stop -Output Diagnostic -PassThru
 if ($PesterResult.Result -ne 'Passed') { Exit 1; return }
