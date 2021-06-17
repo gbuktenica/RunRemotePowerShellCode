2cec0755ae6d0bc3e407ef18127f248a032bbf09
gbuktenica <glen@buktenica.com>
2021-06-17 11:19:26 +0800

    error

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -5,7 +5,7 @@ param(
 Context "Basic Connectivity" {
     Describe "Response" {
         it "should return 0" {
-            $Result = .\Run-RemoteCode.ps1 -Credential $Credential -ScriptBlock -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlockFilePath ".\ScriptBlock.ps1"
+            $Result = .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlockFilePath ".\ScriptBlock.ps1"
             $Result | Should -Be 0 -Because 'This indicates something worked.'
         }
     }
