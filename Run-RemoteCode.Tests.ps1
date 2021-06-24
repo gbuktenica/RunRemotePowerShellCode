572b4d52f0a3656fab68a02e975f568cbb50f30a
Glen Buktenica <glen.buktenica@kloud.com.au>
2021-06-24 10:44:14 +0800

    Something

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -4,9 +4,9 @@ param(
 
 Context "Basic Connectivity" {
     Describe "Response" {
-        it "should return 0" {
-            $Result = .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlockFilePath ".\ScriptBlock.ps1"
-            $Result | Should -Be 0 -Because 'This indicates something worked.'
+        it "should return Something" {
+            $Result = & pwsh -File $PSScriptRoot\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlockFilePath ".\ScriptBlock.ps1"
+            $Result | Should -Be "something" -Because 'This indicates something worked.'
         }
     }
 }
