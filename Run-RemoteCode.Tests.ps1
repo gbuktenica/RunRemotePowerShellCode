2c21c10453517b38e359cb93ce526a2b30acaa69
Glen Buktenica <glen.buktenica@kloud.com.au>
2021-11-23 13:54:04 +0800

    Update test name

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -3,7 +3,7 @@ param(
 )
 describe 'Connect to RA' {
 
-    it 'attempts to create the home folder at the right path' {
+    it 'attempts to connect to the remote computer' {
         $Result = .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlock { Write-Output "Testing" }
         $Result | Should -Be $true
     }
