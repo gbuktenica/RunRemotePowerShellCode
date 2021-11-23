ab6b3d597a0788532a201401eed49ca3418888bf
Glen Buktenica <glen.buktenica@kloud.com.au>
2021-11-23 11:49:39 +0800

    Update to Pester 5

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -3,10 +3,9 @@ param(
 )
 describe 'Change RA' {
 
-    .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlockFilePath ".\ScriptBlock.ps1"
+    .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlock { New-Item $parameters.HomeFolderPath -ItemType "directory" }
 
     it 'attempts to create the home folder at the right path' {
-
-        Test-Path -Path $parameters.HomeFolderPath | should be $true
+        Test-Path -Path $parameters.HomeFolderPath | Should -Be $true
     }
 }
