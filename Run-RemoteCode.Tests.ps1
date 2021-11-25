53ae541f1935888aaa5cee115e93ae866324ace0
Glen Buktenica <glen.buktenica@cewa.edu.au>
2021-11-25 08:30:43 +0800

    SkipDependencies on test

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -4,7 +4,7 @@ param(
 describe 'Connect to RA' {
 
     it 'attempts to connect to the remote computer' {
-        $Result = .\Run-RemoteCode.ps1 -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlock { Write-Output "ArbitraryTestString" }
+        $Result = .\Run-RemoteCode.ps1 -SkipDependencies -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlock { Write-Output "ArbitraryTestString" }
         $Result | Should -Contain 'ArbitraryTestString'
     }
 }
