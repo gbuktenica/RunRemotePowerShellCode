838f320589fc24d282a7e0a6b00451115e0f80e7
Glen Buktenica <glen.buktenica@kloud.com.au>
2021-11-26 09:39:11 +0800

    Add test

--- Run-RemoteCode.Tests.ps1
+++ Run-RemoteCode.Tests.ps1
@@ -2,9 +2,31 @@ param(
     [PsCredential] $Credential
 )
 describe 'Connect to RA' {
-
     it 'attempts to connect to the remote computer' {
-        $Result = .\Run-RemoteCode.ps1 -SkipDependencies -Credential $Credential -Filter 'Name -like "ra*"' -SourceType "Directory" -ScriptBlock { Write-Output "ArbitraryTestString" }
+        $Parameters = @{
+            Credential   = $Credential
+            SourceType   = "Directory"
+            Filter       = 'Name -like "ra*"'
+            ScriptBlock  = { Write-Output "ArbitraryTestString" }
+            FilterScript = { $_.PasswordLastSet -ge ((Get-Date).AddDays(-90)) }
+            SkipDependencies = $true
+        }
+        $Result = .\Run-RemoteCode.ps1 @Parameters
         $Result | Should -Contain 'ArbitraryTestString'
+    }
+}
+describe 'Copy to RA' {
+    it 'attempts to copy files to the remote computer' {
+        $Parameters = @{
+            Credential       = $Credential
+            SourceType       = "Directory"
+            Filter           = 'Name -like "ra*"'
+            ScriptBlock      = { Write-Output "ArbitraryCopyString" }
+            SkipDependencies = $true
+            SourcePath       = ".\working"
+            DestinationPath  = "C:\Windows\Temp"
+        }
+        $Result = .\Run-RemoteCode.ps1 @Parameters
+        $Result | Should -Contain 'ArbitraryCopyString'
     }
 }
